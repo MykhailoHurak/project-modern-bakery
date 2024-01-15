c537c2c039f8c825cafa19f908ab46cb2825354e
mykhailohurak <92752760+MykhailoHurak@users.noreply.github.com>
2024-01-15 18:11:03 +0200

    update

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,7 +2,7 @@ const mobMenu = document.querySelector('.mob-menu');
 const mobMenuBtnOpen = document.querySelector('.mob-menu-btn-open');
 const mobMenuBtnClose = document.querySelector('.mob-menu-btn-close');
 
-const toggleMenu = () => mobMenu.classList.toggle('.is-open');
+const toggleMenu = () => mobMenu.classList.toggle('is-open');
 
 mobMenuBtnOpen.addEventListener('click', toggleMenu);
 mobMenuBtnClose.addEventListener('click', toggleMenu);
