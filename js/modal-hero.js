0f9be6dc536cc5e5e718616b6abab23b3d2a11fb
mykhailohurak <92752760+MykhailoHurak@users.noreply.github.com>
2024-01-25 12:06:18 +0200

    update

--- js/modal-hero.js
+++ js/modal-hero.js
@@ -1,8 +1,8 @@
-const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.js-modal-hero-btn-open');
-const modalBtnClose = document.querySelector('.js-modal-hero-btn-close');
+const modalHero = document.querySelector('.backdrop-hero');
+const modalBtnOpenHero = document.querySelector('.js-modal-hero-btn-open');
+const modalBtnCloseHero = document.querySelector('.js-modal-hero-btn-close');
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+const toggleModalHero = () => modalHero.classList.toggle('is-hidden');
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnClose.addEventListener('click', toggleModal);
+modalBtnOpenHero.addEventListener('click', toggleModalHero);
+modalBtnCloseHero.addEventListener('click', toggleModalHero);
